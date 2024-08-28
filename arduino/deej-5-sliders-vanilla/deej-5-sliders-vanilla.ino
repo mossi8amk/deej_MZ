216f36f4a58c36494ecd4d92dda93da0e8cd7e1a
mossi8amk <143893521+mossi8amk@users.noreply.github.com>
2024-08-28 15:01:41 +0200

    Modified Arduino sketch to include switches and LEDs

--- arduino/deej-5-sliders-vanilla/deej-5-sliders-vanilla.ino
+++ arduino/deej-5-sliders-vanilla/deej-5-sliders-vanilla.ino
@@ -1,9 +1,17 @@
 const int NUM_SLIDERS = 5;
 const int analogInputs[NUM_SLIDERS] = {A0, A1, A2, A3, A4};
+const int switchInputs[NUM_SLIDERS * 2] = {D4, D9, D3, D8, D2, D7, D1, D6, D0, D5};
+const int ledOutputs[NUM_SLIDERS] = {A7, D10, D11, D12, D13};
 
 int analogSliderValues[NUM_SLIDERS];
+char switchValues[NUM_SLIDERS];
+bool ledValues[NUM_SLIDERS];
 
 void setup() { 
+  ledInit();
+  for (int i = 0; i < NUM_SLIDERS * 2; i++) {
+    pinMode(switchInputs[i], INPUT_PULLUP);
+  }
   for (int i = 0; i < NUM_SLIDERS; i++) {
     pinMode(analogInputs[i], INPUT);
   }
@@ -13,7 +21,9 @@ void setup() {
 
 void loop() {
   updateSliderValues();
-  sendSliderValues(); // Actually send data (all the time)
+  updateSwitchValues();
+  updateLED();
+  sendValues(); // Actually send data (all the time)
   // printSliderValues(); // For debug
   delay(10);
 }
@@ -24,14 +34,22 @@ void updateSliderValues() {
   }
 }
 
-void sendSliderValues() {
+void sendValues() {
   String builtString = String("");
 
+  //add slider values to output string
   for (int i = 0; i < NUM_SLIDERS; i++) {
     builtString += String((int)analogSliderValues[i]);
+    builtString += String("|");
+  }
 
+  //add switch values to output string
+  for (int i = 0; i < NUM_SLIDERS; i++) {
+    builtstring += String("S");
+    builtstring += String((char)switchValues[i]);
+    
     if (i < NUM_SLIDERS - 1) {
-      builtString += String("|");
+      builtstring += String("|");
     }
   }
   
@@ -48,5 +66,49 @@ void printSliderValues() {
     } else {
       Serial.write("\n");
     }
+  }
+}
+
+//initializes the LEDs and flashes them as a test
+void ledInit() {
+  for (int i = 0; i < NUM_SLIDERS; i++) {
+    pinMode(ledOutputs[i], OUTPUT);
+    digitalWrite(ledOutputs[i], HIGH);
+    sleep(100);
+    digitalWrite(ledOutputs[i], LOW);
+    sleep(100);
+  }
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < NUM_SLIDERS; j++) {
+      digitalWrite(ledOutputs[j], HIGH);
+    }
+    sleep(100);
+    for (int j = 0; j < NUM_SLIDERS; j++) {
+      digitalWrite(ledOutputs[j], LOW);
+    }
+    sleep(100);
+  }
+}
+
+void updateSwitchValues() {
+  for (int i = 0; i < NUM_SLIDERS * 2; i += 2) {
+    if (digitalRead(switchInputs[i]) == LOW && digitalRead(switchInputs[i+1]) == HIGH) {
+      switchValues[i] = 1;
+      ledValues[i] = false;
+    }
+    else if (digitalRead(switchInputs[i]) == HIGH && digitalRead(switchInputs[i+1]) == LOW) {
+      switchValues[i] = 2;
+      ledValues[i] = false;
+    }
+    else {
+      switchValues[i] = 0;
+      ledValues[i] = true;
+    }
+  }
+}
+
+void updateLED() {
+  for (int i = 0; i < NUM_SLIDERS; i++) {
+    digitalWrite(ledOutputs[i], ledValues[i])
   }
 }
